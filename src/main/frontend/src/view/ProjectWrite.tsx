7cd8728af15de335b3970e1f15992685bb5dc120
hyunho4532 <kyounguk192@gmail.com>
2024-03-06 21:48:18 +0900

    2024-03-04 ExploreNestV2 커밋 (Backend - JsonNode 이용하여 API Mapper)

--- src/main/frontend/src/view/ProjectWrite.tsx
+++ src/main/frontend/src/view/ProjectWrite.tsx
@@ -1,9 +1,33 @@
-function projectWrite() {
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import '../@model/Language'
+import '../@api/response/ApiResponse'
+
+function ProjectWrite() {
+
+    const [languages, setLanguages] = useState<Language[]>([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/fetchData')
+            .then(response => {
+                setLanguages(response.data.language);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }, []);
+
+
     return (
         <div>
-            <h1>테스트 입니다.</h1>
+            <h5>Languages:</h5>
+            <ul>
+                {languages.map(language => (
+                    <li key={language.id}>{language.title}</li>
+                ))}
+            </ul>
         </div>
-    )
+    );
 }
 
-export default projectWrite()
+export default ProjectWrite;
