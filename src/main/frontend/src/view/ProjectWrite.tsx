2b82349c341e08de2cf56ec437d79d2bdacc5ec2
hyunho4532 <kyounguk192@gmail.com>
2024-03-06 23:08:48 +0900

    2024-03-06 ExploreNestV2 커밋 (Frontend - 프로젝트 모집글 쓰기 구현 중)

--- src/main/frontend/src/view/ProjectWrite.tsx
+++ src/main/frontend/src/view/ProjectWrite.tsx
@@ -2,11 +2,25 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import '../@model/Language'
 import '../@api/response/ApiResponse'
+import './scss/ProductWrtie.scss'
+import Select from "react-select";
 
 function ProjectWrite() {
 
     const [languages, setLanguages] = useState([]);
     const [languagesFromData, setLanguagesFromData] = useState('');
+
+    const projectStatus = [
+        { value: "프로젝트", label: "프로젝트" },
+        { value: "스터디", label: "스터디" },
+        { value: "프로젝트/스터디", label: "프로젝트/스터디" },
+    ]
+
+    const [selectProject, setSelectProject] = useState(projectStatus[0]);
+
+    const setSelectOption = (selectedOption: any) => {
+        setSelectProject(selectedOption);
+    }
 
     useEffect(() => {
         axios.get('http://localhost:8080/fetchData')
@@ -24,8 +38,13 @@ function ProjectWrite() {
 
     return (
         <div>
-            <h5>Languages:</h5>
-            { languages.map((value, index) => <li key={index}>{value}</li>)}
+            <h2 id="product-recruitment-title">프로젝트 모집을 원하시나요? 😎😎</h2>
+            <Select
+                id="product-recruitment-select"
+                options={projectStatus}
+                onChange={setSelectOption}
+                placeholder={"프로젝트 진행 방식을 선택해주세요!"}
+            />
         </div>
     );
 }
