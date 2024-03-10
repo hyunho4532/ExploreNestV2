5cd918860fb8dadd42ddbf5684e052567d726115
hyunho4532 <kyounguk192@gmail.com>
2024-03-10 21:44:17 +0900

    2024-03-10 ExploreNestV2 (Frontend - 메인 페이지 Header 구현 중)

--- src/main/frontend/src/App.tsx
+++ src/main/frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import ProjectWrite from "./view/ProjectWrite.tsx";
+import Main from "./view/main/Main.tsx";
 
 function App() {
   return (
       <BrowserRouter>
           <Routes>
+              <Route path="/project/main" element={Main()}></Route>
               <Route path="/project/write" element={ProjectWrite()}></Route>
           </Routes>
       </BrowserRouter>
