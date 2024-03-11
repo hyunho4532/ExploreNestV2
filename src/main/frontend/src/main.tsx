2c0871810fd42562f806b957798960a483e16b35
hyunho4532 <kyounguk192@gmail.com>
2024-03-11 21:48:28 +0900

    2024-03-10 ExploreNestV2 (Frontend - 메인 페이지 구글 로그인 연동)

--- src/main/frontend/src/main.tsx
+++ src/main/frontend/src/main.tsx
@@ -4,11 +4,14 @@ import App from './App.tsx'
 import './index.css'
 import {Provider} from "react-redux";
 import store from "./store.ts";
+import {GoogleOAuthProvider} from "@react-oauth/google";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <Provider store={store}>
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>
-    </Provider>
+    <GoogleOAuthProvider clientId="699075609532-t5vblk4r6usall6vpgaim10di4cvdle1.apps.googleusercontent.com">
+        <Provider store={store}>
+            <React.StrictMode>
+                <App />
+            </React.StrictMode>
+        </Provider>
+    </GoogleOAuthProvider>
 )
--- src/main/frontend/src/view/main/Main.tsx
+++ src/main/frontend/src/view/main/Main.tsx
@@ -1,11 +1,30 @@
 import './scss/Main.scss';
+import {useGoogleLogin} from "@react-oauth/google";
+import axios from "axios";
 
 function Main() {
+
+    const googleSocialLogin = useGoogleLogin({
+        scope: "email profile",
+        onSuccess: async ({ code }) => {
+            axios
+                .post("http://localhost:8085/login/oauth2/code/google", { code })
+                .then(({ data }) => {
+                    console.log(data);
+                });
+        },
+
+        onError: (errorResponse) => {
+            console.error(errorResponse);
+        },
+        flow: "auth-code"
+    })
+
     return (
         <header id="project-main-header">
             <img id="project-main-header-logo" src="../../../public/ExploreNestTitle.jpg" width={160} height={50} />
             <nav id="project-main-menu">
-                <span>로그인</span>
+                <span onClick={googleSocialLogin}>로그인</span>
                 <span onClick={() => window.location.href = "/project/write"}>글 쓰기</span>
                 <span>프로필 정보 조회</span>
             </nav>
